0abdf9191abcaf2bebf3a58751d208ddfbac9327
eriksjoarp <erik.westring@gmail.com>
2022-08-27 16:14:33 +0200

    projConstants contains project specific constants

---
+++ projConstants.py
@@ -0,0 +1,14 @@
+import os
+
+
+
+############################            DIRECTORYS          #####################################
+
+WORKING_DIR = os.getcwd()
+DIR_RESULTS = 'results'
+BASE_DIR_DATASET = r'C:\ai\datasets'
+
+DIR_RESULTS = os.path.join(WORKING_DIR, DIR_RESULTS)
+BASE_DIR_DATASET_CTU = os.path.join(BASE_DIR_DATASET, r'C:\ai\datasets\ctu13\CTU-13-Dataset')
+
+#################################################################################################
