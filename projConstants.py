211bb51786fcbd018718f5c03ec554e08ee14082
eriksjoarp <erik.westring@gmail.com>
2022-10-05 00:19:07 +0200

    Added various new constants

--- projConstants.py
+++ projConstants.py
@@ -1,14 +1,15 @@
 import os
-
-
+import sys
 
 ############################            DIRECTORYS          #####################################
 
 WORKING_DIR = os.getcwd()
-DIR_RESULTS = 'results'
-BASE_DIR_DATASET = r'C:\ai\datasets'
-
-DIR_RESULTS = os.path.join(WORKING_DIR, DIR_RESULTS)
-BASE_DIR_DATASET_CTU = os.path.join(BASE_DIR_DATASET, r'C:\ai\datasets\ctu13\CTU-13-Dataset')
+BASE_DIR_GIT = r'C:\Users\erikw\git'
+BASE_DIR_HELPER = os.path.join(BASE_DIR_GIT, 'helper')
+BASE_DIR_AI_HELPER = os.path.join(BASE_DIR_GIT, 'ai_helper')
+sys.path.append(BASE_DIR_AI_HELPER)
+sys.path.append(BASE_DIR_HELPER)
 
 #################################################################################################
+
+EXPLORE_FILE = 'ctu13_explore.html'
